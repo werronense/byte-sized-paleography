7a18c03ec547c5d0949c3995ad37bf2f31e6a17b
Stephen Werronen <stephen.werronen@gmail.com>
2024-05-29 08:36:27 -0700

    Create page routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<p>landing page</p>} />
+        <Route path="/register" element={<p>registration page</p>} />
+        <Route path="/login" element={<p>login page</p>} />
+        <Route path="/profile" element={<p>profile page</p>} />
+        <Route path="/update-profile" element={<p>update profile page</p>} />
+        <Route path="/transcribe" element={<p>transcribe page</p>} />
+        <Route path="*" element={<p>404 Page Not Found</p>} />
       </Routes>
     </BrowserRouter>
   );
