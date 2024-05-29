e3e91ed5e97619e0ab3c33fda9d8750c74f2ed64
Stephen Werronen <stephen.werronen@gmail.com>
2024-05-29 16:53:07 -0700

    Configure useEffect to fetch data

--- src/pages/TranscriptionPage/TranscriptionPage.jsx
+++ src/pages/TranscriptionPage/TranscriptionPage.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from "react";
+import axios from "axios";
+
 import "./TranscriptionPage.scss";
 
+const { VITE_API_BASE_URL } = import.meta.env;
+
 const TranscriptionPage = () => {
+  useEffect(() => {
+    const getText = async () => {
+      const response = await axios.get(`${VITE_API_BASE_URL}/api/text`);
+      console.log(response.data);
+    }
+    getText();
+  }, []);
+
   return <h1>Transcription Page</h1>;
 };
 
