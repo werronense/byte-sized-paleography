557e21f5e9ef6e07ef61c4672fa2ed5a788b3c7f
Stephen Werronen <stephen.werronen@gmail.com>
2024-05-29 20:32:03 -0700

    Add basic JSX and form handling

--- src/pages/TranscriptionPage/TranscriptionPage.jsx
+++ src/pages/TranscriptionPage/TranscriptionPage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 import "./TranscriptionPage.scss";
@@ -6,15 +6,50 @@ import "./TranscriptionPage.scss";
 const { VITE_API_BASE_URL } = import.meta.env;
 
 const TranscriptionPage = () => {
+  const [text, setText] = useState({});
+  const [userInput, setUserInput] = useState("");
+
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // todo: POST /api/user/:userId/text/:textId
+    // todo: GET /api/text
+  }
+
   useEffect(() => {
     const getText = async () => {
       const response = await axios.get(`${VITE_API_BASE_URL}/api/text`);
-      console.log(response.data);
-    }
+      setText(response.data);
+    };
     getText();
   }, []);
 
-  return <h1>Transcription Page</h1>;
+  useEffect(() => {
+    // reset user input when a new text loads
+    setUserInput("");
+  }, [text]);
+
+  return (
+    text && (
+      <div>
+        <img src={`${VITE_API_BASE_URL}/images/${text.image_url}`} alt="" />
+        <p></p>
+        <form onSubmit={handleSubmit}>
+          <input
+            name="input"
+            type="text"
+            placeholder="Enter Your Transcription"
+            value={userInput}
+            onChange={handleInputChange}
+          ></input>
+          <button type="submit">Next</button>
+        </form>
+      </div>
+    )
+  );
 };
 
 export default TranscriptionPage;
