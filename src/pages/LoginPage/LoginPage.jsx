f1d9796b92cc2344af2278070a79190d0f70d688
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-03 15:00:48 -0700

    Create LoginPage component

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,7 +1,91 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
 import "./LoginPage.scss";
 
+import FormFieldInput from "../../components/FormFieldInput/FormFieldInput";
+
+const { VITE_API_BASE_URL } = import.meta.env;
+
 const LoginPage = () => {
-  return <h1>Login Page</h1>;
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const [formValues, setFormValues] = useState(initialValues);
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const navigate = useNavigate();
+
+  // check whether the form is valid each time values change
+  useEffect(() => {
+    const values = Object.values(formValues);
+
+    // set isFormValid to true if there are no falsy formValues
+    setIsFormValid(!values.map((val) => !!val).includes(false));
+  }, [formValues]);
+
+  // event handlers
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        `${VITE_API_BASE_URL}/api/login`,
+        formValues
+      );
+
+      sessionStorage.setItem("token", response.data.token);
+      navigate("/profile");
+    } catch (err) {
+      console.log(err.response.data.error);
+    }
+  };
+
+  return (
+    <>
+      <h1>Login Page</h1>
+      <form onSubmit={handleSubmit} action="/login">
+        <FormFieldInput
+          labelText="Username: "
+          inputType="text"
+          inputNameId="username"
+          inputPlaceholder="enter your username"
+          inputValue={formValues.username}
+          changeHandler={handleInputChange}
+          isRequired={true}
+        />
+        <FormFieldInput
+          labelText="Password: "
+          inputType="password"
+          inputNameId="password"
+          inputPlaceholder="enter your password"
+          inputValue={formValues.password}
+          changeHandler={handleInputChange}
+          isRequired={true}
+        />
+        <div>
+          <button type="button" onClick={() => navigate("/")}>
+            Cancel
+          </button>
+          <button type="submit" disabled={!isFormValid}>
+            Log In
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default LoginPage;
--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+
 import "./RegistrationPage.scss";
 
 import FormFieldInput from "../../components/FormFieldInput/FormFieldInput";
