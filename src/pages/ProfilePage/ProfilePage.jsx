809072dab5d9b22c126319a09a0759c340cdf7a2
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-03 18:37:38 -0700

    Set up useEffect to fetch data

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -1,7 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const { VITE_API_BASE_URL } = import.meta.env;
+
 import "./ProfilePage.scss";
 
 const ProfilePage = () => {
-  return <h1>Profile Page</h1>;
+  const [user, setUser] = useState({});
+  const [leaderboard, setLeaderboard] = useState([]);
+
+  useEffect(() => {
+    // query database and set user with useEffect on page load
+    const getUser = async () => {
+      const { token } = sessionStorage;
+
+      try {
+        const response = await axios.get(`${VITE_API_BASE_URL}/api/profile`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setUser(response.data);
+      } catch (err) {
+        console.error("GET request to /api/profile failed: ", err);
+      }
+    };
+
+    // query database and set leaderboard with useEffect on page load
+    const getLeaderboard = async () => {
+      try {
+        const response = await axios(`${VITE_API_BASE_URL}/api/leaderboard`);
+        setLeaderboard(response.data);
+      } catch (err) {
+        console.error("GET request to /api/leaderboard failed: ", err);
+      }
+    };
+
+    getLeaderboard();
+    getUser();
+  }, []);
+
+  return (
+    <>
+      <h1>Profile Page</h1>
+      <div></div>
+      <div></div>
+    </>
+  );
 };
 
 export default ProfilePage;
