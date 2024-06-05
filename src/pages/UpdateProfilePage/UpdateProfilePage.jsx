a19cff382d927382f5b3718f512d44745887c893
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-05 08:55:39 -0700

    Add basic JSX for forms

--- src/pages/UpdateProfilePage/UpdateProfilePage.jsx
+++ src/pages/UpdateProfilePage/UpdateProfilePage.jsx
@@ -1,7 +1,77 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 import "./UpdateProfilePage.scss";
 
+import FormFieldInput from "../../components/FormFieldInput/FormFieldInput";
+import Btn from "../../components/Btn/Btn";
+
+const { VITE_API_BASE_URL } = import.meta.env;
+
 const UpdateProfilePage = () => {
-    return <h1>Update Profile</h1>;
-}
+  const initialValues = {
+    username: "",
+    email: "",
+    password: "",
+  };
+
+  const [formValues, setFormValues] = useState(initialValues);
+  const [messages, setMessages] = useState(initialValues);
+
+  // event handlers
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormValues({
+      ...formValues,
+      [name]: value,
+    });
+  };
+
+  return (
+    <>
+      <h1>Update Profile</h1>
+      <form action="/update-profile">
+        <p>{messages.username}</p>
+        <FormFieldInput
+          labelText="New Username"
+          inputType="text"
+          inputNameId="username"
+          inputPlaceholder="Enter a new username"
+          inputValue={formValues.username}
+          changeHandler={handleInputChange}
+          isRequired={true}
+        />
+        <Btn btnType="submit" btnText="Change Username" btnDisabled={false} />
+      </form>
+      <form action="/update-profile">
+        <p>{messages.email}</p>
+        <FormFieldInput
+          labelText="New Email"
+          inputType="email"
+          inputNameId="email"
+          inputPlaceholder="Enter a new email"
+          inputValue={formValues.email}
+          changeHandler={handleInputChange}
+          isRequired={true}
+        />
+        <Btn btnType="submit" btnText="Change Email" btnDisabled={false} />
+      </form>
+      <form action="/update-profile">
+        <p>{messages.password}</p>
+        <FormFieldInput
+          labelText="New Password"
+          inputType="password"
+          inputNameId="password"
+          inputPlaceholder="Enter a new password"
+          inputValue={formValues.password}
+          changeHandler={handleInputChange}
+          isRequired={true}
+        />
+        <Btn btnType="submit" btnText="Change Password" btnDisabled={false} />
+      </form>
+    </>
+  );
+};
 
 export default UpdateProfilePage;
