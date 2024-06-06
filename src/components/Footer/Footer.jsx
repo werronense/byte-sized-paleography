c4b379b5229a87886982d000ed898f25f21429e4
Stephen Werronen <stephen.werronen@gmail.com>
2024-06-06 10:45:46 -0700

    Create site footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import "./Footer.scss";
+
+const Footer = () => {
+  return (
+    <footer className="site-footer">
+      <p className="site-footer__text">
+        <span>Stephanus Werronensis me fecit</span>
+        <span>anno domini mmxxiiij</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
